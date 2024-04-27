90c30978dbb82c6c9549c351a913b72d4c8ebbeb
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-04-27 10:57:13 +0100

    connect ECCONREFUSED resolved, by changing localhost to '127.0.0.1'

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-const express = require("express");
+const express = require("express"); // Import express
+
+const mongoose = require("mongoose") //Import mongoose
 
 const PORT = 8000;
 
+
 // init app
 const app = express();
+
+const Url = "mongodb://127.0.0.1/todoApp"; // connection URL created for the database
+mongoose
+.connect(Url)
+.then(()=> console.log("Database connection successful"))
+.catch((error) => console.log(error.message));
+
+
 
 // view engine
 app.set("view engine", "ejs");
